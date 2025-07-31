dceb64a8fd0811b1455c91afcfea7e3ebdd3965e
Supraja Tangella <supraja.tangella@gmail.com>
2025-07-31 09:11:16 +0100

    Add project files.

---
+++ AzureAdWebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using AzureAdWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AzureAdWebApp.Controllers
+{
+    //[Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Welcome to the internal company portal!";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        //[AllowAnonymous]
+        public IActionResult Public()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
